d4a3cc82afa136a85ffe5cdb0adc9701e18f5030
maria710 <mariakuruczova7@gmail.com>
2022-06-15 13:10:22 +0200

    com6: added activities, fixes

--- app/src/main/java/com/example/quizup/MainActivity.kt
+++ app/src/main/java/com/example/quizup/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.quizup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var startButton: Button? = null
+    private var playButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById<Button>(R.id.buttonPlay)
+
+
+        playButton?.setOnClickListener {
+            setContentView(R.layout.activity_login)
+        }
+
+        startButton = findViewById(R.id.startButton)
+
     }
 }
