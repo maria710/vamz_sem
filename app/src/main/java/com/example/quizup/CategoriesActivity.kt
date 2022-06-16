9fe4fbe2a27ccdc1d4e08029515b7b132f5a9e53
maria710 <mariakuruczova7@gmail.com>
2022-06-16 18:35:11 +0200

    com9: added activities and classes

--- app/src/main/java/com/example/quizup/CategoriesActivity.kt
+++ app/src/main/java/com/example/quizup/CategoriesActivity.kt
@@ -1,7 +1,9 @@
 package com.example.quizup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.GridView
 import androidx.appcompat.widget.Toolbar
 
@@ -12,8 +14,17 @@ class CategoriesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //val toolbar: Toolbar = findViewById(R.id.toolbar)
 
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = "Categories"}
+        //setSupportActionBar(toolbar)
+        //supportActionBar?.title = "Categories"
+    }
+
+    fun history(view: View) {
+        val intend = Intent(this, QuestionActivity::class.java).apply {
+            putExtra("questions", "history")
+        }
+        startActivity(intend)
+        finish()
+    }
 }
--- app/src/main/java/com/example/quizup/QuestionActivity.kt
+++ app/src/main/java/com/example/quizup/QuestionActivity.kt
@@ -2,12 +2,70 @@ package com.example.quizup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 
 class QuestionActivity : AppCompatActivity() {
+
+    private val progress: ProgressBar = findViewById(R.id.progressBar)
+    var numberOfCorrectAns: Int = 0
+    private var listOfQuestion: ArrayList<Question> = Questions.getHistoryQuestions()
+
+    val ans1 = findViewById<Button>(R.id.ans1)
+    val ans2 = findViewById<Button>(R.id.ans2)
+    val ans3 = findViewById<Button>(R.id.ans3)
+    val ans4 = findViewById<Button>(R.id.ans4)
+
+    val question = findViewById<TextView>(R.id.question)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
+        val extra = intent.getStringExtra("questions")
+
+        when(extra) {
+            "history" -> listOfQuestion = Questions.getHistoryQuestions()
+        }
     }
 
+
+    fun ans1(view: View) {
+
+        if (listOfQuestion[progress.progress].correctAnswer == 1) {
+            numberOfCorrectAns++
+            ans1.setBackgroundColor(3)
+        } else {
+            ans1.setBackgroundColor(4)
+        }
+        progress.progress(progress.progress + 1)
+
+    }
+
+    fun ans2(view: View) {
+
+    }
+
+    fun ans3(view: View) {
+
+    }
+
+    fun ans4(view: View) {
+
+    }
+
+    fun next(view: View) {
+        if (progress.progress == progress.max) {
+
+        }
+
+
+        ans1.setText(listOfQuestion[progress.progress].answer1)
+        ans2.setText(listOfQuestion[progress.progress].answer2)
+        ans3.setText(listOfQuestion[progress.progress].answer3)
+        ans4.setText(listOfQuestion[progress.progress].answer4)
+
+    }
 
 }
